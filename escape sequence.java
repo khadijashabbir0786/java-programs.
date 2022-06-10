816f038803dadce4a0428450641c25bee14e4856
khadijashabbir0786 <107266721+khadijashabbir0786@users.noreply.github.com>
2022-06-10 09:48:56 -0700

    escape sequence.java
    add file via upload

---
+++ escape sequence.java
@@ -0,0 +1,10 @@
+public class main
+{
+public static void main(strimgs args[])
+{
+System.out.println("use of escape sequence");
+System.out.println("/1232=1232");
+System.out.println("//u0061=/u0061");
+System.out.println("\t\'program/'/x\  t/");
+}
+}
