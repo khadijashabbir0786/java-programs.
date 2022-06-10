c76cb0717438ab3395b787ddf3f25003358dda60
khadijashabbir0786 <107266721+khadijashabbir0786@users.noreply.github.com>
2022-06-10 10:25:27 -0700

    Add files via upload
    ASCII

---
+++ ASCII.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+public class ASCII 
+{
+  public static void main(String args[])
+{
+ Scanner scan=new Scanner(System.in);
+
+  int n;
+
+System.out.println("Enter your Name:");
+n=scan.nextInt();
+  int a,b,c,d,e,f;
+
+ /*   a=n%10;
+    n=n/10;
+    
+    b=n%10;
+    n=n/10;
+    
+    c=n%10;
+    n=n/10;
+    
+    d=n%10;
+    n=n/10;
+    
+    e=n%10;
+    n=n/10;
+    
+    f=n%10;
+    n=n/10;
+
+System.out.println("ASCII code is:" +f +e +d +c +b +a); */
+
+
+
+
+}
+
+
+}
