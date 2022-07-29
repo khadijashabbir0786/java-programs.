abf14ac61c17d7bb2c1b0b6db064dc4c4052ed59
khadijashabbir0786 <107266721+khadijashabbir0786@users.noreply.github.com>
2022-07-29 09:39:43 -0700

    javac

---
+++ maindemoarrayavg.java
@@ -0,0 +1,19 @@
+class maindemoarrayavg
+{
+public static void main(string arg[])
+{
+int[] m=new int [5];
+for(int i=0; i<m.length; i++)
+{
+m[i]=arg[i]; 
+}
+for(int i=0; i<m.length; i++)
+}
+System.out.println(m[i]);
+for(int i=0; i<m.length; i++)
+{
+sum=sum+m[i];
+}
+avg=(double)sum/m.length;
+System.out.println("average: "+avg);
+}
